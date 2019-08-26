2e7b63a53ef14a6251d5b7f81da3fe20a5cbc0b8
ob-sc <54313166+ob-sc@users.noreply.github.com>
2019-08-26 14:46:04 +0200

    JS in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 moment.locale('de')
 
 let choices, loehne, kennung, name, datum, beginnForm, endeForm, diffMinuten, gehalt, daten;
+let tage, summe;
 
 // namen, löhne und station für alle
 $.get("../scripts/getdata.php", function(data){
@@ -99,36 +100,123 @@ function formBerechnung() {
 // ABRECHNUNG
 function abtabelle() {
     // TODO WICHTIG URLAUBSTAGE
-    // TODO zeilen zusammenfassen? (zb <thead> und <tr>)
-    let html = '<h3 style="text-align:center">Monatsabrechnung ' + station + ', ' + moment($('#datum').val(), 'YYYY-MM').format('MMMM YYYY') + '</h3>\n';
-    html += '<table class="table table-bordered" style="width:100%">\n';
-    html += '<thead>\n';
-    html += '<tr>\n';
+    let html = '<h3 style="text-align:center">Monatsabrechnung ' + station + ', ' + moment($('#datum').val(), 'YYYY-MM').format('MMMM YYYY') + '</h3>';
+    html += '<table class="table table-bordered" style="width:100%"><thead><tr>';
     // th style="width:x%"
-    html += '<th scope="col">PN</th>\n';
-    html += '<th scope="col">Name</th>\n';
-    html += '<th scope="col">Arbeitszeit</th>\n';
-    html += '<th scope="col">Gehalt</th>\n';
-    html += '<th scope="col">Tage</th>\n';
-    html += '</tr>\n';
-    html += '</thead>\n';
-    html += '<tbody>\n';
+    html += '<th scope="col">PN</th>';
+    html += '<th scope="col">Name</th>';
+    html += '<th scope="col">Arbeitszeit</th>';
+    html += '<th scope="col">Gehalt</th>';
+    html += '<th scope="col">Tage</th>';
+    //html += '<th scope="col">Urlaubstage</th>';
+    html += '</tr></thead><tbody>';
     for (let x in daten) {
         let gehalt = daten[x].gehalt;
+        // Wenn Gehalt im Monat über 450 -> Zeile Rot
         if (daten[x].gehalt > 450) {
-            html += '<tr class="table-danger">\n';
+            html += '<tr class="table-danger">';
         } else {
-            html += '<tr>\n';
+            html += '<tr>';
         }
-        html += '<th scope="row">' + daten[x].personalnr + '</th>\n';
+        html += '<th scope="row">' + daten[x].personalnr + '</th>';
         html += '<td>' + daten[x].name + '</td>\n';
-        html += '<td>' + moment.utc().startOf('day').add(daten[x].arbeitszeit, 'minutes').format('HH:mm') + '</td>\n';
-        html += '<td>' + gehalt.toFixed(2) + '</td>\n';
-        html += '<td>' + daten[x].tage + '</td>\n';
-        html += '</tr>';
+        html += '<td>' + moment.utc().startOf('day').add(daten[x].arbeitszeit, 'minutes').format('HH:mm') + '</td>';
+        html += '<td>' + gehalt.toFixed(2) + '</td>';
+        html += '<td>' + daten[x].tage + '</td></tr>';
+        // noch urlaubstage
     }
-    $('#atext').html(html + '</tbody>\n</table>\n<input type="button" onclick="window.print();" value="Drucken" class="noPrint btn scc">');
+    $('#atext').html(html + '</tbody></table><input type="button" onclick="window.print();" value="Drucken" class="noPrint btn scc">');
 }
+
+// AUSWERTEN
+function eatabelle() {
+    let eintragVorher, gehaltEA, seVorhanden;
+    let sonderRow = ' ';
+    // Ende Funktion wenn keine Einträge
+    if (tage.length == 0) {
+        $('#eaText').html('<h3>Keine Einträge für diesen Monat</h3>');
+        return;
+    }
+    // Tage im Monat
+    let monatstage = moment($('#datum').val(), "YYYY-MM").daysInMonth();
+    // Funktion normaler Eintrag
+    function tagZeile(row) {
+        gehaltEA = tage[row].gehalt;
+        html += '<tr><th scope="row">' + momentTag + '</th>';
+        html += '<td>' + tage[row].beginn + '</td>';
+        html += '<td>' + tage[row].ende + '</td>';
+        html += '<td>' + moment.utc().startOf('day').add(tage[row].arbeitszeit, 'minutes').format('HH:mm') + '</td>';
+        html += '<td>' + gehaltEA.toFixed(2) + '</td></tr>';
+    }
+    // Funktion Sondereintrag bei mehrfachem Datum
+    function sonderZeile(row) {
+        gehaltEA = tage[row].gehalt
+        sonderRow += '<tr><th scope="row">' + momentTag + '</th>';
+        sonderRow += '<td>' + tage[row].beginn + '</td>';
+        sonderRow += '<td>' + tage[row].ende + '</td>';
+        sonderRow += '<td>' + moment.utc().startOf('day').add(tage[row].arbeitszeit, 'minutes').format('HH:mm') + '</td>';
+        sonderRow += '<td>' + gehaltEA.toFixed(2) + '</td></tr>';
+    }
+    // Variable mit String für Tabelle
+    let html = '<h3 style="text-align:center">Monatsübersicht ' + $('#nameInput').val() + ', ' + moment($('#datum').val(), 'YYYY-MM').format('MMMM YYYY') + '</h3>\n';
+    html += '<table class="table table-bordered table-sm" style="width:100%"><thead><tr>';
+    html += '<th style="width:20%" scope="col">Tag</th>';
+    html += '<th style="width:20%" scope="col">Beginn</th>';
+    html += '<th style="width:20%" scope="col">Ende</th>';
+    html += '<th style="width:20%" scope="col">Arbeitszeit</th>';
+    html += '<th style="width:20%" scope="col">Gehalt</th></tr></thead><tbody>';
+    // Loop für alle Monatstage
+    for (let i = 1; i <= monatstage; i++) {
+        let eintrag = false;
+        // Loop für Objekt mit Tagen aus eaget.php
+        for (let x in tage) {
+            momentTag = moment(tage[x].datum, 'YYYY-MM-DD').format('D');
+            // console.log('x: ' + x + ', i: ' + i + ', momentTag: ' + momentTag + ', tage[x].datum: ' + tage[x].datum + ', eintragVorher: ' + eintragVorher); TODO nur test
+            // Normaler Eintrag
+            if (i == momentTag && eintragVorher != momentTag) {
+                tagZeile(x);
+                eintragVorher = momentTag;
+                eintrag = true;
+                delete tage[x];
+                break;
+            // Sondereintrag
+            } else if (momentTag == eintragVorher) {
+                seVorhanden = true;
+                eintrag = true;
+                sonderZeile(x);
+                delete tage[x];
+                break;
+            }
+        }
+        // Leerer Eintrag wenn davor nichts eingetragen wurde
+        if (eintrag == false) {
+            html += '<tr><th scope="row">' + i + '</th><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td></tr>';
+        }
+    }
+    // Wiedergeben der Tabelle
+    $('#eaText').html(html + '</tbody></table>');
+    // Tabelle für Sondereinträge
+    let sonderEintrag = '<h3 style="text-align:center;">Sondereinträge</h3>';
+    sonderEintrag += '<table class="table table-bordered table-sm" style="width:100%;"><thead><tr>';
+    sonderEintrag += '<th style="width:20%" scope="col">Tag</th>';
+    sonderEintrag += '<th style="width:20%" scope="col">Beginn</th>';
+    sonderEintrag += '<th style="width:20%" scope="col">Ende</th>';
+    sonderEintrag += '<th style="width:20%" scope="col">Arbeitszeit</th>';
+    sonderEintrag += '<th style="width:20%" scope="col">Gehalt</th></tr></thead><tbody>';
+    sonderEintrag += sonderRow;
+    // Wenn die Tabelle Sondereinträge nicht leer ist diese wiedergeben
+    if (sonderRow.length > 1) {
+        $('#eaText').append(sonderEintrag + '</tbody></table>');
+    }
+    // Zusammenrechnung des Monats aus eaget.php
+    $('#eaText').append('<strong>Arbeitszeit:</strong> ' + moment.utc().startOf('day').add(summe['SUM(arbeitszeit)'], 'minutes').format('HH:mm'));
+    let sumGehalt = summe['SUM(gehalt)'];
+    $('#eaText').append('<br><strong>Gehalt:</strong> ' + sumGehalt.toFixed(2) + '€');
+    $('#eaText').append('<br><strong>Arbeitstage:</strong> ' + summe['COUNT(DISTINCT datum)'] + '<br>');
+    
+    // Druckbutton
+    $('#eaText').append('<input type="button" onclick="window.print();" value="Drucken" class="noPrint btn scc my-3">');
+};
 
 $(document).ready(function() {
 
@@ -163,6 +251,27 @@ $(document).ready(function() {
             $('#atext').html('<h3>Anfrage fehlgeschlagen</h3><br><h5>Bitte <a href="mailto:bergen@starcar.de">Ole Bergen</a> kontaktieren</h5>')
         })
     })
+
+    // AUSWERTEN
+    $('#eaform').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'eaget.php',
+            type: 'POST',
+            dataType: 'json',
+            data: $("#eaform").serialize(),
+        })
+        .done(function(data) {
+            daten = data;
+            tage = daten.tage;
+            summe = daten.summe;
+            console.log(daten);
+            eatabelle();
+        })
+        .fail(function() {
+            $('#eaText').html('<h3>Anfrage fehlgeschlagen</h3><br><h5>Bitte <a href="mailto:bergen@starcar.de">Ole Bergen</a> kontaktieren</h5>')
+        })
+    });
 
     // für jeden input Datum - automatisch Datum heute
     document.getElementById('datum').valueAsDate = new Date();
