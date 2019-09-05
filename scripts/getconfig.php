f8596b482743c55c1c352b1aedeb1b6c45ef4d3e
ob-sc <54313166+ob-sc@users.noreply.github.com>
2019-09-05 17:15:08 +0200

    config

---
+++ scripts/getconfig.php
@@ -0,0 +1,34 @@
+<?php
+
+// Rückgabe vorbereiten
+$daten = [
+    'status' => 'OK',
+    'daten' => [],
+    'meldung' => '',
+];
+
+// Pfad setzen
+$iniFile = __DIR__ . '/../config.ini';
+
+// prüfen ob existiert und lesbar
+if(!file_exists($iniFile) || !is_readable($iniFile)) {
+    $daten['status'] = 'ERROR';
+    $daten['meldung'] = 'Konfigdatei nicht vorhanden oder nicht lesbar';
+    die(json_encode($daten));
+}
+
+// Datei parsen
+$config = parse_ini_file($iniFile, true);
+
+if($config === false) {
+    $daten['status'] = 'ERROR';
+    $daten['meldung'] = 'Konfigdatei nicht parsbar';
+    die(json_encode($daten));
+}
+
+// Sicherheit: sql-Daten entfernen
+unset($config['sql']);
+
+// Rückgabe der Konfig
+$daten['daten'] = $config;
+echo json_encode($daten);
