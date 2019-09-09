2d6f5c8845d7ee68d5af245699f42640d48ffba8
ob-sc <54313166+ob-sc@users.noreply.github.com>
2019-09-09 11:10:22 +0200

    ma fertig

--- js/main.js
+++ js/main.js
@@ -572,4 +572,35 @@ $(document).ajaxComplete(function() {
             $(this).blur();
         }
     })
+
+    // MITARBEITER bearbeiten
+    let maRow;
+    for (let x in maDaten) {
+        if (maDaten[x].status == 'neu') {
+            maRow += '<tr class="table-danger"><td>' + maDaten[x].username + '</td>';
+            maRow += '<td>' + maDaten[x].status + '</td>';
+            maRow += '<th><img src="../img/confirm.svg" width="18" class="confirm" id="' + maDaten[x].id + '"></th></tr>';
+        } else {
+            maRow += '<tr><td>' + maDaten[x].username + '</td>';
+            maRow += '<td>Bestätigt</td>';
+            maRow += '<td>&nbsp</td></tr>'
+        }
+    }
+    $('#maTab').html(maRow);
+
+    $('.confirm').click(function() {
+        let id = $(this).attr('id');
+        $.ajax({
+            url: 'maedit.php',
+            method: 'POST',
+            data: {id: id}
+        })
+        .done(function() {
+            location.reload();
+        })
+        .fail(function(data) {
+            $('#fehlerText').html('<strong>Fehler:</strong>' + data.responseText);
+            $('#fehlerAlert').show();
+        })
+    })
 })
--- mitarbeiter/index.php
+++ mitarbeiter/index.php
@@ -5,8 +5,8 @@ include '../req/header.php';
 
 <h2 style="text-align:center">Mitarbeiter</h2>
 
-<div class="container" id="maContainer">
-    <table class="table table-hover table-sm" style="width:50%;margin:auto;">
+<div id="maContainer" style="width:50%;margin:auto;border: 1px solid;">
+    <table class="table table-hover table-sm">
         <thead>
             <tr>
                 <th style="width:60%">Name</th>
@@ -18,39 +18,3 @@ include '../req/header.php';
         </tbody>
     </table>
 </div>
-
-
-<script>
-$(document).ajaxComplete(function() {
-    // MITARBEITER bearbeiten
-    let maRow;
-    for (let x in maDaten) {
-        if (maDaten[x].status == 'neu') {
-            maRow += '<tr class="table-danger"><td>' + maDaten[x].username + '</td>';
-            maRow += '<td>' + maDaten[x].status + '</td>';
-            maRow += '<th><img src="../img/confirm.svg" width="18" class="confirm" id="' + maDaten[x].id + '"></th></tr>';
-        } else {
-            maRow += '<tr><td>' + maDaten[x].username + '</td>';
-            maRow += '<td>Bestätigt</td>';
-            maRow += '<td>&nbsp</td></tr>'
-        }
-    }
-    $('#maTab').html(maRow);
-
-    $('.confirm').click(function() {
-        let id = $(this).attr('id');
-        $.ajax({
-                url: 'maedit.php',
-                method: 'POST',
-                data: {id: id}
-            })
-            .done(function() {
-                location.reload();
-            })
-            .fail(function(data) {
-                $('#fehlerText').html('<strong>Fehler:</strong>' + data.responseText);
-                $('#fehlerAlert').show();
-            })
-    })
-});
-</script>
