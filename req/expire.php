6f26230b31bbb012a2f79a51988a6d52e13dd6f1
ob-sc <54313166+ob-sc@users.noreply.github.com>
2019-09-09 15:32:10 +0200

    expire 180

--- req/expire.php
+++ req/expire.php
@@ -4,7 +4,7 @@ session_start();
 if(isset($_SESSION['aktiv'])){
     $sekInaktiv = time() - $_SESSION['aktiv'];
     // in sekunden, 3 Min (180)
-    $expireNach = 1800000;
+    $expireNach = 180;
     if($sekInaktiv >= $expireNach){
         session_unset();
         session_destroy();
