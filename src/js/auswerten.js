557b84b0cce0eb0dd7a49e30336ea57a22a12dca
ob-sc <ob-sc@DESKTOP-BURJHOI.localdomain>
2019-11-29 10:30:22 +0100

    dev build

--- src/js/auswerten.js
+++ src/js/auswerten.js
@@ -114,7 +114,7 @@ function eatabelle() {
   // prettier-ignore
   html = `<h3 style="text-align:center">Arbeitszeitnachweis ${$('#auswertenAuto').val()}<br>${moment(monatfuerTage, 'M').format('MMMM')}-${moment($('#datum').val(),'YYYY-MM').format('MMMM YYYY')}</h3>`;
   html +=
-    '<table class="table table-bordered table-sm"><caption>Gelb = In anderer Station gearbeitet</caption><thead><tr>';
+    '<table class="table table-bordered table-sm"><caption>Gelb = In anderer Station gearbeitet<br>Rot = Notdienst</caption><thead><tr>';
   html += '<th style="width:20%">Tag</th>';
   html += '<th style="width:20%">Beginn</th>';
   html += '<th style="width:20%">Ende</th>';
