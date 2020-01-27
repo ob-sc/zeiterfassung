0bbd5b2dba8cb107c35314c8f1ad28280b85105e
ob-sc <ole.bergen.92@gmail.com>
2020-01-27 16:58:08 +0100

    auswerten auch andere stationen

--- public/api/eaget.php
+++ public/api/eaget.php
@@ -9,12 +9,11 @@ $endDate = new DateTime($_POST['datum'].'-16');
 #10.juli bis 9. august ist august
 
 // QUERY 1 mehrere Reihen
-$zeitenSql = "SELECT datum, beginn, ende, arbeitszeit, gehalt, station, ahstation FROM zeiten WHERE ahid = :id AND ahstation = :station AND datum BETWEEN :beginnDate AND :endDate ORDER BY datum ASC, beginn ASC";
+$zeitenSql = "SELECT datum, beginn, ende, arbeitszeit, gehalt, station, ahstation FROM zeiten WHERE ahid = :id AND datum BETWEEN :beginnDate AND :endDate ORDER BY datum ASC, beginn ASC";
 
 $stmt = $conn->prepare($zeitenSql);
 
 $stmt->bindValue(':id', $_POST['id']);
-$stmt->bindValue(':station', $_SESSION['station']);
 $stmt->bindValue(':beginnDate', $beginnDate->format('Y-m-d'));
 $stmt->bindValue(':endDate', $endDate->format('Y-m-d'));
 
@@ -23,12 +22,11 @@ $stmt->execute();
 $zeiten = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // QUERY 2 Zähl-Funktionen
-$sumSql = "SELECT SUM(arbeitszeit) AS arbeitszeit, SUM(gehalt) AS gehalt, COUNT(DISTINCT datum) AS datum FROM zeiten WHERE ahid = :id AND ahstation = :station AND datum BETWEEN :beginnDate AND :endDate";
+$sumSql = "SELECT SUM(arbeitszeit) AS arbeitszeit, SUM(gehalt) AS gehalt, COUNT(DISTINCT datum) AS datum FROM zeiten WHERE ahid = :id AND datum BETWEEN :beginnDate AND :endDate";
 
 $stmt = $conn->prepare($sumSql);
 
 $stmt->bindValue(':id', $_POST['id']);
-$stmt->bindValue(':station', $_SESSION['station']);
 $stmt->bindValue(':beginnDate', $beginnDate->format('Y-m-d'));
 $stmt->bindValue(':endDate', $endDate->format('Y-m-d'));
 
