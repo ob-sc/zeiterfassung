3fe2359f1123a40ff46e97d80b2f513620ad559f
ob-sc <bergen@starcar.de>
2021-03-16 02:14:10 +0100

    purge

--- public/api/selectStation.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-$_SESSION['station'] = $_POST['newStation'];
-
-echo($_SESSION['station']);
--- public/api/wedelete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require '../scripts/connect.php';
-
-$stmt = $conn->prepare("DELETE FROM wochenende WHERE id = ?");
-
-$stmt->execute(array($_POST['id']));
-
-$conn = null;
